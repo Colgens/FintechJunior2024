10b4bef2ba5b4881e8bf64a9f038d3cb23262b02
colgens <colgens@gmail.com>
2024-09-03 17:34:31 +0700

    refactor: Улучшение интеграции Jackson и Lombok
    - Добавлена зависимость для обеспечения корректной десериализации объектов Jackson с использованием Lombok.
    - Удалены излишние аннотации JsonProperty из классов Location и Coords для упрощения кода.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.2")
     implementation("org.projectlombok:lombok:1.18.34")
+    annotationProcessor ("org.projectlombok:lombok:1.18.34")
     implementation("org.slf4j:slf4j-api:2.0.16")
     implementation("ch.qos.logback:logback-classic:1.5.7")
 }
--- src/main/java/com/tbank/fintech_project/model/entities/Location.java
+++ src/main/java/com/tbank/fintech_project/model/entities/Location.java
@@ -1,13 +1,10 @@
 package com.tbank.fintech_project.model.entities;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Location {
-    @JsonProperty
     private String slug;
-    @JsonProperty
     private Coords coords;
 }
